39ba4a47e460036580a5cf596c8884f1a74880b6
2bitbit <180839704+2bitbit@users.noreply.github.com>
2025-12-19 20:20:18 +0800

    fix: fixed the logic of the HNU adapter

--- docs/adapters/湖南大学.js
+++ docs/adapters/湖南大学.js
@@ -23,7 +23,7 @@
 
         static parse(rows) {
             let output = [];
-            const weekMap = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"];
+            const weekMap = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
 
             const cleanLocation = (raw) => {
                 let cleaned = raw;
